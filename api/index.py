ea44616b3f11c0d0fad3b93cf30ac2129beb0bac
topitobravo <d_reinoso9@hotmail.com>
2025-03-31 03:08:55 +0200

    adds deps2

--- api/index.py
+++ api/index.py
@@ -357,4 +357,4 @@ def unauthorized(e):
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) 
